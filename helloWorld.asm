12689bbfe7f89835852f2d7710868077b5efe1c0
slowy07 <slowy.arfy@gmail.com>
2020-10-27 05:48:11 +0700

    hello world

--- helloWorld.asm
+++ helloWorld.asm
@@ -3,4 +3,15 @@ section .text
 
 _start:
   mov edx, len
+  mov ecx, msg ;message write to program
+  mov ebx, 1
+  mov eax, 4
+  int 0x80 ;kernel
+
+  mov eax, 1 ;for exit
+  int 0x80 ;kernel
+  
+section .data
+msg db 'Hello world', 0xa ;output
+len equ $ -msg 
   
